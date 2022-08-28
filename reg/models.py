6aa11fe9759887138114bf299730f14b703c3025
anton <anton@piksolaris.ru>
2022-08-28 23:43:43 +0500

    status

--- reg/models.py
+++ reg/models.py
@@ -2,6 +2,13 @@ from django.db import models
 from django.shortcuts import render, redirect
 
 class reg(models.Model):
+    class Stat(models.Model):
+        STATUS = (
+            ('создан', 'создан'),
+            ('зарегистрирован', 'зарегистрирован'),
+            ('согласован', 'согласован'),
+            ('завершен', 'завершен')
+        )
 
     nombers = models.CharField('Номер проекта', max_length=10)
     NameProg = models.CharField('Наименование', max_length=100)
@@ -9,7 +16,7 @@ class reg(models.Model):
     Responsible = models.CharField('Отвественный', max_length=100)
     Customer_company = models.IntegerField('Организация заказчик ИНН', max_length=10)
     Adres_obj = models.TextField('Адрес объекта')
-    Status = models.CharField('Статус', max_length=15, default='создан')
+    Status = models.CharField('Статус', max_length=30, default='создан', choices=Stat.STATUS)
 
     # название строчки в таблице
     def __str__(self):
